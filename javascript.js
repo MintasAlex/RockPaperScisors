e30aaef474f2739a0bf67914329b3bfcc0fdea0e
Alex Mintas <alexmintas@gmail.com>
2022-10-25 16:50:53 +0300

    Squashed commit of the following:
    commit 54f5f89f235169dc21485312d71f0c3fc96f05cf
    Author: Alex Mintas <alexmintas@gmail.com>
    Date:   Tue Oct 25 16:49:09 2022 +0300
    
        Disabled choice buttons during gameOver state
    
    commit 10d45375e371abe085b8df68aae5b1263bedc3bb
    Author: Alex Mintas <alexmintas@gmail.com>
    Date:   Tue Oct 25 16:42:31 2022 +0300
    
        gameOver refactor
    
    commit 2862042225ef2049e3a10a555a05994649285513
    Author: Alex Mintas <alexmintas@gmail.com>
    Date:   Tue Oct 25 16:17:21 2022 +0300
    
        UI + Best/5 refactor

--- javascript.js
+++ javascript.js
@@ -1,7 +1,20 @@
-function getComputerChoice(){
-    let randomChoice = Math.floor(Math.random()*3);
-    // console.log(randomChoice);
-    switch(randomChoice){
+const choiceMenu = document.querySelector('.choice-menu');
+const btns = choiceMenu.querySelectorAll('button');
+const resultDisplay = document.querySelector(".result");
+const playerScoreDisplay = document.querySelector("#playerScore");
+const computerScoreDisplay = document.querySelector("#computerScore");
+const roundDisplay = document.querySelector("#round");
+const gameOverContainer = document.querySelector("#gameOver");
+const gameOverMsg = gameOverContainer.querySelector('h2');
+const resetGameBtn = gameOverContainer.querySelector('button');
+
+
+
+let playerScore = computerScore = round = 0;
+
+function getComputerChoice() {
+    let randomChoice = Math.floor(Math.random() * 3);
+    switch (randomChoice) {
         case 0:
             return ("rock");
             break;
@@ -17,49 +30,86 @@ function getComputerChoice(){
     }
 }
 
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
-    if (playerSelection === computerSelection){
+    if (playerSelection === computerSelection) {
         return ("It's a tie!")
-    }else if (playerSelection == "rock"){
-        if (computerSelection == "scisors"){
+    } else if (playerSelection == "rock") {
+        if (computerSelection == "scisors") {
             return ("You win! Rock beats Scisors!");
-        }else return ("You lose! Paper beats Rock!");
-    }else if (playerSelection == "paper"){
-        if (computerSelection == "rock"){
+        } else return ("You lose! Paper beats Rock!");
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "rock") {
             return ("You win! Paper beats Rock!");
-        }else return ("You lose! Scisors beats Paper!");       
-    }else if (computerSelection == "paper"){
+        } else return ("You lose! Scisors beats Paper!");
+    } else if (computerSelection == "paper") {
         return ("You win! Scisors beats Paper!");
-    }else return ("You lose! Rock beats Scisors!");
+    } else return ("You lose! Rock beats Scisors!");
 }
 
-function game(){
-    let result; 
-    let playerScore = computerScore = 0;
-    for (let i = 0; i < 5; i++){
-        playerSelection = prompt("Choose one: Rock, Paper or Scisors!");
-        computerSelection = getComputerChoice();
-        result = playRound(playerSelection, computerSelection);
-        console.log("Round "+(i+1)+"! "+result);
-        if (result.substring(4,7) === "win")
-            playerScore++;
-        else if (result.substring(4,8) === "lose")
-            computerScore++;
 
-        console.log(playerScore +" - "+computerScore);
-    }
-    switch(true){
-        case (playerScore > computerScore):
-            console.log("Player wins!");
-            break;
-        case (playerScore < computerScore):
-            console.log("Computer wins!");
-            break;
-        default:
-            console.log("It's a tie!");
-    }
-    
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    round = 0;
+    updateDisplay();
+    resultDisplay.textContent = "Choose one:";
+    gameOverContainer.style.display = 'none';
+    btns.forEach(btn => btn.disabled = false);
+
 }
 
-game();
+function gameOver(playerScore, computerScore) {
+    if (playerScore > computerScore) {
+        gameOverMsg.textContent = "You won!";
+    } else {
+        gameOverMsg.textContent = "You lost!";
+    }
+
+    gameOverContainer.style.display = 'block';
+
+    resetGameBtn.addEventListener('click', () => {
+        resetGame();
+    });
+
+    btns.forEach(btn => btn.disabled = true);
+
+
+}
+
+function updateDisplay() {
+    roundDisplay.textContent = "Round: " + round;
+    playerScoreDisplay.textContent = "Player Score: " + playerScore;
+    computerScoreDisplay.textContent = "Computer Score: " + computerScore;
+}
+
+function game(playerSelection) {
+    let result;
+    round++;
+    playerSelection = playerSelection.toLowerCase();
+    console.log(playerSelection);
+    computerSelection = getComputerChoice();
+    result = playRound(playerSelection, computerSelection);
+    resultDisplay.textContent = result;
+    if (result.substring(4, 7) === "win") {
+        playerScore++;
+    } else if (result.substring(4, 8) === "lose") {
+        computerScore++;
+    }
+    updateDisplay();
+
+    if (playerScore == 5 || computerScore == 5) {
+        gameOver(playerScore, computerScore);
+    }
+}
+
+btns.forEach(btn => btn.addEventListener('click', () => {
+    game(btn.textContent);
+}));
+
+
+
+
+
+
